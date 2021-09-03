c5079912b6621a1c998d698bea438e25fdfdb9d6
wonkooklee <const.wonkook@gmail.com>
2021-09-03 12:27:17 +0900

    Modified: Updated Logic

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-const getEvenNumbers = (nums) => {
+const getEvenNumbers = (init, max) => {
 	const result = [];
-	for(let i=2; i<=50; i+=2) {
+	for(let i=init%2 ? init+1 : init; i <= max; i+=2) {
 		result.push(i);
 	};
 	return result;
 };
 
-getEvenNumbers()
+getEvenNumbers(1, 50)
 // Expected result: [ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50 ]
